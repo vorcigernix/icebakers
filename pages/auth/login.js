36c2e6f4c4ef4280a1e03fabc361a31a01d550c3
Adam Sobotka <vorcigernix@gmail.com>
2021-02-09 20:56:06 +0100

    login redesign

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -5,38 +5,64 @@ export default function SignIn({ providers }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Sign-in to IceBakers</title>
+        <title>IceBakers</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-extrabold">
-          <span className="text-blue-600">Ice</span>
+          🧁
+          <span className="text-blue-400">Ice</span>
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-600">
             Bakers
           </span>
         </h1>
-
-        <p className="my-6 text-2xl">Get to know your colleagues and friends</p>
-
-        <div className="flex flex-col flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <>
+        <>
+          <div className="md:flex p-8">
             {Object.values(providers).map((provider) => (
               <div key={provider.name}>
                 <button
-                  className="w-full flex items-center justify-center px-3 py-3 border border-transparent text-base font-medium rounded-md text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 shadow-sm"
+                  className="flex items-center justify-center px-3 py-3 border border-transparent text-base font-medium rounded-md text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 shadow-sm m-4"
                   onClick={() => signIn(provider.id)}
                 >
                   Sign in with {provider.name}
                 </button>
               </div>
             ))}
-          </>
-
-          <span className="text-gray-900 w-2/3 pt-8">
-            We use your login to recognize your colleagues. Please use your work account.
-          </span>
-        </div>
+          </div>
+          <div className="md:flex p-8 md:p-0">
+            <img
+              className="w-1/3 h-32 md:w-48 md:h-auto mx-auto"
+              src="/bg2.svg"
+              alt=""
+            />
+            <div className="w-2/3 pt-6 md:p-8 text-center md:text-left space-y-4">
+              <p className="my-6 text-2xl">
+                Get to know your colleagues and friends
+              </p>
+              <p className="text-lg font-light">
+                IceBakers works in two stages. In one, you respond to the
+                "icebreaker questions" and your responses are saved with your
+                name. In a second stage, you will get the questions with a reply
+                from your colleague. There is the catch though, you will get two
+                names and your goal is to figure out who is the right person
+                that wrote this answer.
+              </p>
+              <p className="text-lg font-light">
+                This site is heavily inspired by a functionality of&nbsp;
+                <a
+                  href="https://www.quizbreaker.com/"
+                  className="font-semibold"
+                >
+                  QuizBreaker
+                </a>
+                . We want to create a similar system that will be free to use,
+                with a reward system that will allow you to appreciate
+                interesting and honest answers of your friends.
+              </p>
+            </div>
+          </div>
+        </>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
