c04a5df8dd08f5b65a27200a3d7aba82a973b232
Adam Sobotka <vorcigernix@gmail.com>
2021-02-18 10:56:11 +0100

    questions to route

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
                   </p>
                   <div className="md:flex pb-8">
                     <Wallet session={session} />
-                    <a href="/answerquestions">
+                    <a href="/questions/687656586">
                       <button className="flex items-center justify-center text-base font-medium text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 mr-4 shadow rounded border-0 p-3">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
