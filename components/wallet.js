7a878ee62760e0d8b2ad7b635915b563cfda567d
Adam Sobotka <vorcigernix@gmail.com>
2021-02-18 19:41:00 +0100

    walletonboard

--- components/wallet.js
+++ components/wallet.js
@@ -1,7 +1,7 @@
 import Web3Container from "../lib/Web3Container";
 import { getPendingTips, connectWallet } from "../lib/tipsService";
 import React, { useEffect, useState } from "react";
-import { useSession } from 'next-auth/client'
+import { useSession } from "next-auth/client";
 
 async function tipFriend({ friend, amount, contract, wallet, web3 }) {
   console.log(amount);
@@ -41,37 +41,44 @@ function WalletComponent(props) {
   const [wallet, setWallet] = React.useState(
     props.accounts && props.accounts[0]
   );
-  
+
   const [pendingTips, setPendingTips] = React.useState(false);
   const [session] = useSession();
   const [timer, setTimer] = useState(0);
 
-  useEffect((e) => {
-    // poll the pending tips
-    console.log(session);
-    if (session.user && session.user.address) {
-      window.clearInterval(timer);
-      setTimer(0);
-      return; // we don't need to check pending tips if the user already exists
-    }
-    setTimer(window.setInterval(async (e) => {
-      const result = await getPendingTips();
-      setPendingTips(+result.pending > 0);
-    }, 15000));
-  }, [session.user.address]);
+  useEffect(
+    (e) => {
+      // poll the pending tips
+      console.log(session);
+      if (session.user && session.user.address) {
+        window.clearInterval(timer);
+        setTimer(0);
+        return; // we don't need to check pending tips if the user already exists
+      }
+      setTimer(
+        window.setInterval(async (e) => {
+          const result = await getPendingTips();
+          setPendingTips(+result.pending > 0);
+        }, 15000)
+      );
+    },
+    [session.user.address]
+  );
 
   return (
     <>
       {pendingTips && (
         <button
-          className="flex items-center justify-center text-base font-medium text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 mr-4 shadow rounded border-0 p-3"
+          className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-green-50"
           onClick={(e) => registerWalletToClaim(wallet)}
         >
+          <span>Connect wallet</span>
           <svg
+            className="h-5 w-5"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            className="h-8 mx-2 text-blue-400"
+            aria-hidden="true"
           >
             <path
               fillRule="evenodd"
@@ -79,8 +86,25 @@ function WalletComponent(props) {
               clipRule="evenodd"
             />
           </svg>
-          Claim your tips
         </button>
+        // <button
+        //   className="flex items-center justify-center text-base font-medium text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 mr-4 shadow rounded border-0 p-3"
+        //   onClick={(e) => registerWalletToClaim(wallet)}
+        // >
+        //   <svg
+        //     xmlns="http://www.w3.org/2000/svg"
+        //     viewBox="0 0 20 20"
+        //     fill="currentColor"
+        //     className="h-8 mx-2 text-blue-400"
+        //   >
+        //     <path
+        //       fillRule="evenodd"
+        //       d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
+        //       clipRule="evenodd"
+        //     />
+        //   </svg>
+        //   Claim your tips
+        // </button>
       )}
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -249,25 +249,7 @@ export default function Home() {
                         />
                       </svg>
                     </button>
-                    <button
-                      className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-green-50"
-                      onClick={() => setPageIndex(pageIndex + 1)}
-                    >
-                      <span>Connect wallet</span>
-                      <svg
-                        className="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </button>
+                    <Wallet />
                   </nav>
                 </div>
               </div>
