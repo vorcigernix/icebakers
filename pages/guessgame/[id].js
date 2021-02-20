427d23cf1d931791127e3c7052c8da65c4189a97
Adam Sobotka <vorcigernix@gmail.com>
2021-02-20 08:43:41 +0100

    fakeit

--- pages/guessgame/[id].js
+++ pages/guessgame/[id].js
@@ -10,8 +10,254 @@ export default function GuessGame() {
   if (!loading && !session?.user) return signin();
 
   const fetcher = (url, id) => fetch(url, id).then((r) => r.json());
-  const { data, error } = useSWR(() => id && `/api/getanswers/${id}`, fetcher);
-  console.log(data);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  //  const { data, error } = useSWR(() => id && `/api/getanswers/${id}`, fetcher);
+  //console.log(data);
+  //if (error) return <div>failed to load</div>;
+  //if (!data) return <div>loading...</div>;
+
+  //get the questions so I don't need to fake them
+  const { data, error } = useSWR(`/api/questions`, fetcher);
+  const anwers = [
+    {
+      answer: "No. And yes, I know I am not getting rewarded for this.",
+    },
+    {
+      answer: "No. And yes, I know I am not getting rewarded for this.",
+    },
+    {
+      answer: "ACTIV which is like active, which is not me",
+    },
+    {
+      answer: "Can'ŧ say there is.",
+    },
+    {
+      answer:
+        "Definitely. We don't matter. It is how far we can go as a humankind, before the sun burn us down that matters.",
+    },
+    {
+      answer: "Makumba. Ok, no, not really.",
+    },
+    {
+      answer: "Definitely.",
+    },
+    {
+      answer: "What? I am famous!",
+    },
+    {
+      answer:
+        "Cat. In my house. I actually thought that I am lazy before I've got cat.",
+    },
+    {
+      answer:
+        "So, I've done a lot of them. I feel really bad for jumping over begging homeless person, but I was really drunk. Yes, I know it doesn't help.",
+    },
+    {
+      answer:
+        "A beautiful chunk of wood from my 3yrs old kid, wrapped in some leather. Neither of us can say what it is (he is now 16)",
+    },
+    {
+      answer:
+        "That \"no matter how hard you try, in the end it doesn't even matter\". But that's not just now, it is the way I perceive the life. ",
+    },
+    {
+      answer:
+        "Pear on pizza. Try it. Not my invention, had one in northern Italy and it is great.",
+    },
+    {
+      answer:
+        "Prague. Seriously, I've been in many places, I love a lot of them, but I am always glad to return.",
+    },
+    {
+      answer: "Talker that try hard to be a listener too.",
+    },
+    {
+      answer:
+        "I will be jumping around the world, feeling guilty about damage I do to the nature.",
+    },
+    {
+      answer: "Armenia. And no, I don't know why.",
+    },
+    {
+      answer:
+        "I wanted to be a film director, like my father. So, kinda dreams fulfilled, I am a director :-) ",
+    },
+    {
+      answer: "Hora rut.",
+    },
+    {
+      answer: "I will burn it doing nothing probably. ",
+    },
+    {
+      answer:
+        "I was almost answering my wife, but I wouldn't do that to her. Other than that, I'd need just one thing: a satellite phone.",
+    },
+    {
+      answer: "ACTIV, which is most certainly not what I am!",
+    },
+    {
+      answer: "Yes I would visit all the planets and spend my days on uranus",
+    },
+    {
+      answer:
+        "No, but my dad is always being confused with someone (Jackie Chan)",
+    },
+    {
+      answer: "Cats, they are carefree and their own boss",
+    },
+    {
+      answer:
+        "Accidentally left my whatsapp open in a meeting with all my bosses",
+    },
+    {
+      answer:
+        "Accidentally left my whatsapp open in a meeting with all my bosses",
+    },
+    {
+      answer: "Its my life!",
+    },
+    {
+      answer: "Its my life!",
+    },
+    {
+      answer: "Inside my own head ",
+    },
+    {
+      answer: "Did you ask something?",
+    },
+    {
+      answer: "I'd like to visit every continent",
+    },
+    {
+      answer:
+        "In a small town in Kuala Lumpur on a large piece of land doing simple farming",
+    },
+    {
+      answer:
+        "In a small town in Kuala Lumpur on a large piece of land doing simple farming",
+    },
+    {
+      answer: "Buy bitcoin, buy ethereum, hold at least 1000 of each",
+    },
+    {
+      answer: "i would most likely waste it!",
+    },
+    {
+      answer: "Knife, Glass, Plastic Wrap",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col  min-h-screen py-2">
+      <main className="flex flex-col items-center mt-9 flex-1 px-20">
+        <h1 className="text-6xl font-extrabold">
+          🧁
+          <span className="text-blue-400">Ice</span>
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-600">
+            Bakers
+          </span>
+        </h1>
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg md:mt-14 w-2/3">
+          {data.map((item, index) => (
+            <div
+              className={
+                index == questionIndex ? `visible px-4 py-5 sm:px-6` : `hidden`
+              }
+              key={item.id}
+            >
+              <h3 className="text-4xl font-extrabold  text-gray-900">
+                {item.questiontext}
+              </h3>
+            </div>
+          ))}
+
+          <div>
+            <div className="mt-1 py-2 mx-4 rounded-md ring-2 ring-blue-400 ">
+              <textarea
+                id="answer"
+                name="answer"
+                rows="3"
+                className="block w-full resize-none p-2 outline-none"
+                placeholder="type your answer and click next"
+                autoFocus
+                onChange={(event) => setAnswerText(event.target.value)}
+                value={answerText}
+              ></textarea>
+            </div>
+          </div>
+          <nav
+            className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px m-6"
+            aria-label="Pagination"
+          >
+            <button
+              className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+              onClick={() => setQuestionIndex(questionIndex - 1)}
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+
+            {questionIndex > 15 && (
+              <button
+                className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-green-100 text-sm font-medium text-gray-500 hover:bg-green-50"
+                onClick={() => handleClick()}
+              >
+                <span>Guess Answers</span>
+                <svg
+                  className="h-5 w-5 mx-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            )}
+
+            <button
+              className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-green-50"
+              onClick={() => handleClick()}
+            >
+              <span>Next</span>
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            <Wallet session={session} />
+          </nav>
+          <p className=" px-4 pb-6 font-light">
+            Now you can guess the answers. You will see what other people in
+            your company responded and you will have to guess who gave this
+            answer.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
