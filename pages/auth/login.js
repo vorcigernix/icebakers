150f85bb9453dc3cc7bf5ffe26c6ce6b24fdf3b5
Adam Sobotka <vorcigernix@gmail.com>
2021-03-05 08:31:36 +0100

    login mobile fix

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -3,41 +3,45 @@ import { providers, signIn } from "next-auth/client";
 
 export default function SignIn({ providers }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>IceBakers</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={`nojs-show loaded`}>
+      <noscript>
+        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+      </noscript>
+      <div className="flex flex-col min-h-screen py-2">
+        <Head>
+          <title>IceBakers</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="flex flex-col items-center mt-9 flex-1 px-20">
-        <h1 className="text-6xl font-extrabold">
-          🧁
-          <span className="text-blue-400">Ice</span>
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-600">
-            Bakers
-          </span>
-        </h1>
-        <>
-          <div className="md:flex p-8">
-            {Object.values(providers).map((provider) => (
-              <div key={provider.name}>
-                <button
-                  className="flex items-center justify-center px-3 py-3 border border-transparent text-base font-medium rounded-md text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 shadow-sm m-4"
-                  onClick={() => signIn(provider.id)}
-                >
-                  Sign in with {provider.name}
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className="md:flex p-8 md:p-0">
-            <img
-              className="w-1/3 h-32 md:w-48 md:h-auto mx-auto"
-              src="/bg2.svg"
-              alt=""
-            />
-            <div className="w-2/3 pt-6 md:p-8 text-center md:text-left space-y-4">
-              <p className="my-6 text-2xl">
+        <main className="flex flex-col items-center mt-9 flex-1 md:px-20">
+          <h1 className="text-6xl font-extrabold text-center md:text-left">
+            🧁
+            <span className="text-blue-400">Ice</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-600">
+              Bakers
+            </span>
+          </h1>
+          <>
+            <div className="md:flex p-8">
+              {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                  <button
+                    className="flex items-center justify-center px-3 py-3 border border-transparent text-base font-medium rounded-md text-gray-600 bg-white hover:bg-gray-50 hover:text-black md:py-3 md:px-3 shadow-sm m-4"
+                    onClick={() => signIn(provider.id)}
+                  >
+                    Sign in with {provider.name}
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className="text-center md:text-left md:flex">
+              <img
+                className="hidden w-1/2 h-auto mx-auto md:block"
+                src="/bg2.svg"
+                alt="Illustration"
+              />
+
+              <p className="my-6 text-xl mx-auto">
                 Get to know your colleagues and friends
               </p>
               <p className="text-lg font-light">
@@ -61,20 +65,20 @@ export default function SignIn({ providers }) {
                 interesting and honest answers of your friends.
               </p>
             </div>
-          </div>
-        </>
-      </main>
+          </>
+        </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://gitcoin.co/issue/binancex/Grant-projects/17/100024656"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          built at Binance Hackathon: The Future Is Now
-        </a>
-      </footer>
+        <footer className="flex items-center justify-center w-full h-24 md:border-t">
+          <a
+            className="flex items-center justify-center"
+            href="https://gitcoin.co/issue/binancex/Grant-projects/17/100024656"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            built at Binance Hackathon: The Future Is Now
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
