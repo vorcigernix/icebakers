4bb86b3c8bf270ba8fe46a93389b2bce22327bb0
Victa <victa.phu@gmail.com>
2021-04-10 00:14:21 +0800

    feat: added contract-kit package and docker (but doesn't seem to work...)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
 import Head from "next/head";
+import { ContractKitProvider } from '@celo-tools/use-contractkit';
+import '@celo-tools/use-contractkit/lib/styles.css';
 
 function MyApp({ Component, pageProps }) {
   const { session } = pageProps;
@@ -12,32 +14,34 @@ function MyApp({ Component, pageProps }) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧁</text></svg>"
         />
-                  <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            rel="preload"
-            as="style"
-            href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Lato:wght@300;400;700;900&display=swap"
-          />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Lato:wght@300;400;700;900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Lato:wght@300;400;700;900&display=swap"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <noscript>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Lato:wght@300;400;700;900&display=swap"
-            media="print"
-            onLoad="this.media='all'"
           />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:wght@700&family=Lato:wght@300;400;700;900&display=swap"
-            />
-          </noscript>
+        </noscript>
       </Head>
-      <Provider session={session}>
-        <Component {...pageProps} />
-      </Provider>
+      <ContractKitProvider dappName="IceBakers">
+        <Provider session={session}>
+          <Component {...pageProps} />
+        </Provider>
+      </ContractKitProvider>
     </>
   );
 }
